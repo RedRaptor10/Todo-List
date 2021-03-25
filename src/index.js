43964b0cf439fbdc4662edc38b28397219a6454f
RedRaptor10 <weiminh_lam@yahoo.com>
2021-03-25 18:57:38 -0500

    Add cursor

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import projectFactory from './project.js';
 import taskFactory from './task.js';
 import loadSidebar from './sidebar.js';
 
+let projects = [];
+
 loadProjects();
 initialize(projects);
 loadHeader();
 loadSidebar(projects);
 
 function loadProjects() {
-    let projects;
-
     // If local storage is not empty, load projects from local storage
     if (localStorage.getItem('projects')) {
         projects = JSON.parse(localStorage.getItem('projects'));
